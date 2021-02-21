5899aef0e3b38a7776df4322f49567c8406c4595
cjl72 <cjl72@njit.edu>
2021-02-21 16:49:43 -0500

    Task: Had to Create new Project

--- productList.js
+++ productList.js
@@ -1,26 +1,26 @@
 (function () {
     var mockDB = [
-        { make: 'BMW', model: 'M2', year: '2021', price: 70000},
-        { make: 'Audi', model: 'RS5', year: '2021', price: 100000},
-        { make: 'BMW', model: 'M4', year: '2021', price: 100000},
-        { make: 'Mercedes', model: 'AMG GT', year: '2021', price: 400000},
-        { make: 'BMW', model: 'i8', year: '2021', price: 150000},
-        { make: 'Porsche', model: '911 Turbo', year: '2021', price: 170000},
-        { make: 'Lamborghini', model: 'Huracan', year: '2021', price: 230000},
-        { make: 'BMW', model: '7 Series', year: '2021', price: 100000},
-        { make: 'Aston Martin', model: 'Vantage', year: '2021', price: 140000},
+        { make: 'BMW', model: 'M2', year: '2021', price: 70000, img: ''},
+        { make: 'Audi', model: 'RS5', year: '2021', price: 100000, img: ''},
+        { make: 'BMW', model: 'M4', year: '2021', price: 100000, img: ''},
+        { make: 'Mercedes', model: 'AMG GT', year: '2021', price: 400000, img: ''},
+        { make: 'BMW', model: 'i8', year: '2021', price: 150000, img: ''},
+        { make: 'Porsche', model: '911 Turbo', year: '2021', price: 170000, img: ''},
+        { make: 'Lamborghini', model: 'Huracan', year: '2021', price: 230000, img: ''},
+        { make: 'BMW', model: '7 Series', year: '2021', price: 100000, img: ''},
+        { make: 'Aston Martin', model: 'Vantage', year: '2021', price: 140000, img: ''},
     ];
 
     function renderList (results) {
         var items = document.querySelector('#products');
-        items.innerHTML = '';
         var cars = results.map(function (result) {
-            return (result.year + " " + row.model + " " + row.make + " $" + row.price.toString())
+            return (result.year + " " + result.model + " " + result.make + " $" + result.price.toString())
         })
         cars.forEach(function (row) {
             var newDiv = document.createElement("div");
             newDiv.className = 'card';
-            toAdd.appendChild(newDiv)
+            newDiv.innerHTML = row.toString();
+            items.appendChild(newDiv);
         });
     }
 
